92b7f9542d5e53cedc7cd90603afece5faa00924
normansophie789 <10008389+normansophie789@users.noreply.github.com>
2024-10-04 12:52:43 +0100

    Fixing syntax errors

--- src/utils/ovenToAirfryerConverter.js
+++ src/utils/ovenToAirfryerConverter.js
@@ -4,13 +4,13 @@ const FAHRENHEIGHT_ADJUST = 25;
 
 export const convertTime = (time) => {
     if (time <= 0) return null;
-    return Math.floor(ovenTime * 0.8);
+    return Math.floor(time * 0.8);
 }
 
 export const convertTemp = (temp, ovenType, units) => {
     if (temp <= 40) return null;
-    if (!['fan', 'conventional'].contains(ovenType)) return null;
-    if (!['c', 'f'].contains(units)) return null;
+    if (!['fan', 'conventional'].includes(ovenType)) return null;
+    if (!['c', 'f'].includes(units)) return null;
 
     let adjust = units == 'c' ? CELSIUS_ADJUST : FAHRENHEIGHT_ADJUST;
     let multiplier = ovenType == 'fan' ? FAN_MULTIPLIER : 1;
