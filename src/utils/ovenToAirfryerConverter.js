94a578102106c2bd3c83f7ff32506c60d6f06e76
normansophie789 <10008389+normansophie789@users.noreply.github.com>
2024-10-04 20:31:42 +0100

    Adding extra validation for time

--- src/utils/ovenToAirfryerConverter.js
+++ src/utils/ovenToAirfryerConverter.js
@@ -3,7 +3,7 @@ const CELSIUS_ADJUST = 20;
 const FAHRENHEIGHT_ADJUST = 25;
 
 export const convertTime = (time) => {
-    if (time <= 0) return null;
+    if (!time || typeof time !== 'number' || time <= 0) return null;
     return Math.floor(time * 0.8);
 }
 
