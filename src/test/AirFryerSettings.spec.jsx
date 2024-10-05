a448ed1043b4567931945d41008b77bde69e450c
normansophie789 <10008389+normansophie789@users.noreply.github.com>
2024-10-05 13:29:35 +0100

    Adding air fryer component test

---
+++ src/test/AirFryerSettings.spec.jsx
@@ -0,0 +1,14 @@
+import { expect, test, describe, vi } from 'vitest';
+import { render, screen, fireEvent } from "@testing-library/react";
+import AirFryerSettings from '../components/AirFryerSettings';
+
+describe('AirFryerSettings', () => {
+    beforeEach(() => {
+        render(<AirFryerSettings temp={200} time={10} unit={'*C'} />)
+    });
+
+    test('it should render the AirFryerSettings component', () => {
+        expect(screen.getByText('Temperature: 200*C')).toBeInTheDocument();
+        expect(screen.getByText('Duration: 10 minutes')).toBeInTheDocument();
+    })
+});
