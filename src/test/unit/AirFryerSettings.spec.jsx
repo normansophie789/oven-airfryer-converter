a33696441ced82a8a23568f43a286b823515dd6a
normansophie789 <10008389+normansophie789@users.noreply.github.com>
2024-10-05 13:58:30 +0100

    Fixing lint config

--- src/test/unit/AirFryerSettings.spec.jsx
+++ src/test/unit/AirFryerSettings.spec.jsx
@@ -1,5 +1,5 @@
 import { expect, test, describe } from 'vitest';
-import { render, screen, beforeEach } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import AirFryerSettings from '../../components/AirFryerSettings';
 
 describe('AirFryerSettings', () => {
